242457b49689201f764608b039618f520b2d17bc
altb <altb@zhaw.ch>
2024-03-26 13:53:01 +0100

    Solution Week 6

--- realtime_thread.cpp
+++ realtime_thread.cpp
@@ -10,7 +10,7 @@ realtime_thread::realtime_thread(IO_handler *io, float Ts) : thread(osPriorityHi
     m_io->disable_escon();
     ti.reset();
     ti.start();
-    Integrator = IIR_filter(Ts);
+    Integrator = IIR_filter(Ts); // Instanziere Objekt fuer Integrator
     }
 
 // decontructor for controller loop
@@ -20,8 +20,8 @@ realtime_thread::~realtime_thread() {}
 // this is the main loop called every Ts with high priority
 void realtime_thread::loop(void){
 
-    float K[2] {-1.39251,-0.08642};
-    float K4[4] {-1.71203,-0.15652,-0.00284,0.00172};
+    float K[2] {-1.39251,-0.08642}; // Zustandsmatrix fuer Modellierung 2ter Ordnung
+    float K4[4] {-1.71203,-0.15652,-0.00284,0.00172};   // Modell 3Ord, + Integrator
     float km = 36.9e-3;             // Motorkonstante
     float M_soll,i_soll;
     while(1)
@@ -33,7 +33,7 @@ void realtime_thread::loop(void){
             {
                 /* Aufgabe 5.1: Zustand x_1 = Winkel, x_2 = Winkelgeschw. */
             //    M_soll = -(K[0] * m_io->get_phi_bd() + K[1] * m_io->get_gz() );
-                M_soll = -(K4[0] * m_io->get_phi_bd() + K4[1] * m_io->get_gz() 
+                M_soll = -(K4[0] * m_io->get_phi_bd() + K4[1] * m_io->get_gz()          // Zustandsregler
                          + K4[2] * m_io->get_vphi_fw()+ K4[3] * Integrator(0 - m_io->get_vphi_fw()) );
                 i_soll = M_soll/km;
                 m_io->write_current(i_soll);
